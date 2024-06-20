a5d7be919983515c6a620d81aff0bd2cdd19499e
elie <inahon.elie17@yahoo.com>
2024-06-20 02:08:19 -0400

    Upgraded Project

--- src/main/java/com/eliehome/officeInformation/OfficeInformationApplication.java
+++ src/main/java/com/eliehome/officeInformation/OfficeInformationApplication.java
@@ -1,4 +1,4 @@
-package com.eliehome.staffmanagement;
+package com.eliehome.officeInformation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
