e9a31c0b76f51e0211bb71c11b412e2597c93fde
elie <inahon.elie17@yahoo.com>
2024-06-20 02:10:46 -0400

    Upgraded Project

---
+++ src/main/java/com/eliehome/officeInformation/error/OfficeNotFoundException.java
@@ -0,0 +1,23 @@
+package com.eliehome.officeInformation.error;
+
+public class OfficeNotFoundException extends Exception {
+    public OfficeNotFoundException() {
+        super();
+    }
+
+    public OfficeNotFoundException(String message) {
+        super(message);
+    }
+
+    public OfficeNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OfficeNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected OfficeNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
