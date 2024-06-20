e522e94185ae8701027c77537b2f25bcb4b3eb0b
elie <inahon.elie17@yahoo.com>
2024-06-20 02:15:35 -0400

    Upgraded Project

---
+++ src/main/java/com/eliehome/officeInformation/repository/OfficeRepository.java
@@ -0,0 +1,12 @@
+package com.eliehome.officeInformation.repository;
+
+import com.eliehome.officeInformation.entity.Office;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfficeRepository extends JpaRepository<Office, Long> {
+    public Office findByManager(String manager);
+    public Office findByManagerIgnoreCase(String manager);
+
+}
