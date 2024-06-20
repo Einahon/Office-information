4fc29fda7d9fe734be6859df00e15d4d9f48750f
elie <inahon.elie17@yahoo.com>
2024-06-20 02:04:53 -0400

    upgrade project

--- src/test/java/com/eliehome/officeInformation/StaffManagementApplicationTests.java
+++ src/test/java/com/eliehome/officeInformation/StaffManagementApplicationTests.java
@@ -1,6 +1,5 @@
-package com.eliehome.staffmanagement;
+package com.eliehome.officeInformation;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
