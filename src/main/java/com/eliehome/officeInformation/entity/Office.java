8150f3396bf6615e067948bf04a6a0e38798710a
elie <inahon.elie17@yahoo.com>
2024-06-22 18:18:52 -0400

    new commit and update

--- src/main/java/com/eliehome/officeInformation/entity/Office.java
+++ src/main/java/com/eliehome/officeInformation/entity/Office.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,14 +20,17 @@ public class Office {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long office_id;
+
     @Length(max=50, min = 0)
     private String address;
+
     @Length(max=50, min = 0)
     private String city;
+
     @Length(max=50, min = 0)
     private String state;
-    @NotBlank(message = "Please add Office Manager")
-    @Length(max=50, min = 0)
+
+
     private String manager;
 
 
