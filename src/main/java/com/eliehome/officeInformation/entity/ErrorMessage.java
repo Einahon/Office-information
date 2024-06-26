453f69b615f0b81e88f33c90c748f6fb9bbbfdf7
elie <inahon.elie17@yahoo.com>
2024-06-25 21:02:47 -0400

    Handler Not_Found Exception

--- src/main/java/com/eliehome/officeInformation/entity/ErrorMessage.java
+++ src/main/java/com/eliehome/officeInformation/entity/ErrorMessage.java
@@ -1,14 +1,13 @@
-//package com.eliehome.officeInformation.entity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ErrorMessage {
-//
-//    private HttpStatus status;
-//    private String message;
-//}
+package com.eliehome.officeInformation.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private HttpStatus status;
+    private String message;
+}
