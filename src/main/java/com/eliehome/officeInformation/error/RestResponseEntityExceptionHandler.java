d2a732b1823509e1df93911227b172f156150f72
elie <inahon.elie17@yahoo.com>
2024-06-25 21:04:25 -0400

    Handler Not_Found Exception

--- src/main/java/com/eliehome/officeInformation/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/eliehome/officeInformation/error/RestResponseEntityExceptionHandler.java
@@ -1,23 +1,21 @@
-//package com.eliehome.officeInformation.error;
-//
-//import com.eliehome.officeInformation.entity.ErrorMessage;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.context.request.WebRequest;
-//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-//@ControllerAdvice
-//@ResponseStatus
-//public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
-//@ExceptionHandler(OfficeNotFoundException.class)
-//    public ResponseEntity<ErrorMessage> officeNotFoundException(OfficeNotFoundException exception,
-//                                                               WebRequest request){
-//    ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
-//            exception.getMessage());
-//
-//    return ResponseEntity.status(HttpStatus.NOT_FOUND)
-//            .body(message);
-//}
-//}
+package com.eliehome.officeInformation.error;
+
+import com.eliehome.officeInformation.entity.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+@ControllerAdvice
+@ResponseStatus
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(OfficeNotFoundException.class)
+    public ResponseEntity<ErrorMessage> officeNotFoundException(OfficeNotFoundException exception, WebRequest request){
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
+                exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(message);
+    }
+}
