ef77b417a0161db0f75cb0074817cbfeb0c07c4e
elie <inahon.elie17@yahoo.com>
2024-06-27 14:11:27 -0400

    Handler Not_Found Exception

--- src/main/java/com/eliehome/officeInformation/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/eliehome/officeInformation/error/RestResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package com.eliehome.officeInformation.error;
 import com.eliehome.officeInformation.entity.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,12 +11,21 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 @ControllerAdvice
 @ResponseStatus
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+public class RestResponseEntityExceptionHandler  {
     @ExceptionHandler(OfficeNotFoundException.class)
-    public ResponseEntity<ErrorMessage> officeNotFoundException(OfficeNotFoundException exception, WebRequest request){
+    public ResponseEntity<ErrorMessage> employeeNotFoundException(OfficeNotFoundException exception,
+                                                                  WebRequest request){
         ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND,
                 exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(message);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> employeeFieldValidationError(MethodArgumentNotValidException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessage(HttpStatus.BAD_REQUEST,
+                exception.getBindingResult().getFieldError().getDefaultMessage()));
+    }
+
+
 }
