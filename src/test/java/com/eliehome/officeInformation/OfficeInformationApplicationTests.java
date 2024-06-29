b7e0714141077a2db1ba5f33c21789b8272ec0d1
elie <inahon.elie17@yahoo.com>
2024-06-29 13:49:50 -0400

    Refactoring

--- src/test/java/com/eliehome/officeInformation/OfficeInformationApplicationTests.java
+++ src/test/java/com/eliehome/officeInformation/OfficeInformationApplicationTests.java
@@ -3,7 +3,7 @@ package com.eliehome.officeInformation;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StaffManagementApplicationTests {
+class OfficeInformationApplicationTests {
 
 
 
