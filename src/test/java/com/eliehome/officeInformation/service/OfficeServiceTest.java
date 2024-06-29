c72f3d2b6c38e98175b5f6ebbb130b0b7ff2d8d9
elie <inahon.elie17@yahoo.com>
2024-06-29 14:12:55 -0400

    Test office Service

--- src/test/java/com/eliehome/officeInformation/service/OfficeServiceTest.java
+++ src/test/java/com/eliehome/officeInformation/service/OfficeServiceTest.java
@@ -1,6 +1,7 @@
 package com.eliehome.officeInformation.service;
 
 import com.eliehome.officeInformation.entity.Office;
+import com.eliehome.officeInformation.error.OfficeNotFoundException;
 import com.eliehome.officeInformation.repository.OfficeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,30 +10,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class OfficeServiceTest {
     @Autowired
 private OfficeService officeService;
-//    @MockBean
-//    private OfficeRepository officeRepository;
-//    @BeforeEach
-//    void setUp() {
-//        Office office =
-//                Office.builder()
-//                        .staffName("David Diva")
-//                        .staffDepartment("Prod")
-//                        .staffPhoneNumber("513-303-0040")
-//                        .staffId(1L)
-//                        .build();
-//
-//        Mockito.when(officeRepository.findByStaffNameIgnoreCase("David Diva"))
-//                .thenReturn(office);
-//    }
-//    @Test
-//
-//    public void whenValidStaffName_thenDepartmentIsCorrect(){
-//        String staffName = "David Diva";
-//        Office found = officeService.fetchStaffByName(staffName);
-//        assertEquals(staffName, found.getStaffName());
-//    }
+    @MockBean
+    private OfficeRepository officeRepository;
+    @BeforeEach
+    void setUp() {
+        Office office =
+                Office.builder()
+                        .address("03 Reinke Trail")
+                        .state("OH")
+                        .city("Cincinnati")
+                        .manager("Dossou Joseph")
+                        .office_id(1L)
+                        .build();
+
+        Mockito.when(officeRepository.findByManagerIgnoreCase("Dossou Joseph"))
+                .thenReturn(office);
+    }
+    @Test
+
+    public void whenValidManager_thenOfficeIsCorrect() throws OfficeNotFoundException {
+        String manager = "Dossou Joseph";
+        Office found = officeService.fetchOfficeByManager(manager);
+        assertEquals(manager, found.getManager());
+    }
 }
