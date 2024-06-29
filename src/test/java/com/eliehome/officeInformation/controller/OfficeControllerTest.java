b25485bcc9792f7182268675913d839382a4be74
elie <inahon.elie17@yahoo.com>
2024-06-29 13:51:33 -0400

    Test controller

--- src/test/java/com/eliehome/officeInformation/controller/OfficeControllerTest.java
+++ src/test/java/com/eliehome/officeInformation/controller/OfficeControllerTest.java
@@ -14,65 +14,77 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@WebMvcTest(StaffController.class)
-//
-//class OfficeControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
-//    private OfficeService officeService;
-//
-//    private Office office;
-//
-//    @BeforeEach
-//    void setUp(){
-//
-//        office = Office.builder()
+@WebMvcTest(OfficeController.class)
+
+class OfficeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OfficeService officeService;
+
+    private Office office;
+
+    @BeforeEach
+    void setUp(){
+
+        office = Office.builder()
+                .address("07 driveways")
+                .state("Taxes")
+                .city("Huston")
+                .manager("Sohounde Emmanuel")
+                .office_id(1L)
+                .build();
 //                .staffPhoneNumber("513-303-0040")
 //                .staffDepartment("Prod")
 //                .staffName("David Diva")
 //                .staffId(1L)
 //                .build();
-//    }
+    }
 //
 //
-//    @Test
-//    void saveStaff() throws Exception {
-//       Office inputOffice = Office.builder()
+    @Test
+    void saveOffice() throws Exception {
+       Office inputOffice = Office.builder()
+                .address("07 driveways")
+                .state("Taxes")
+                .city("Huston")
+                .manager("Sohounde Emmanuel")
+                .build();
 //                .staffPhoneNumber("513-700-0990")
 //                .staffDepartment("CE")
 //                .staffName("Deborah Kpakpo")
 //                .build();
 //
-//       Mockito.when(officeService.saveStaff(inputOffice))
-//               .thenReturn(office);
+       Mockito.when(officeService.saveOffice(inputOffice))
+               .thenReturn(office);
+
+       mockMvc.perform(post("/offices")
+               .contentType(MediaType.APPLICATION_JSON)
+               .content("{\n" +
+                      "\t\"address\":\"07 driveways\",\n" +
+                       "\t \"manager\":\"Sohounde Emmanuel\",\n" +
+                       "\t\t\"city\": \"Huston\",\n" +
+                       "\t\t\"state\": \"Taxes\"\n" +
+                       "\t\n" +
+                       "}"))
+               .andExpect(status().isOk());
+    }
 //
-//       mockMvc.perform(post("/staffs")
-//               .contentType(MediaType.APPLICATION_JSON)
-//               .content("{\n" +
-//                       "\t \"staffName\":\"Deborah Kpakpo\",\n" +
-//                       "\t\t\"staffDepartment\": \"CE\",\n" +
-//                       "\t\t\"staffPhoneNumber\": \"513-700-0990\"\n" +
-//                       "\t\n" +
-//                       "}"))
-//               .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    void fetchStaffById() throws Exception {
-//        Mockito.when(officeService.fetchStaffById(1L))
-//                .thenReturn(office);
-//
-//        mockMvc.perform(get("/staffs/1")
-//                .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.staffName").value(office.getStaffName()));
-//
-//
-//
-//
-//
-//
-//    }
-//}
+    @Test
+    void fetchOfficeById() throws Exception {
+        Mockito.when(officeService.fetchOfficeById(1L))
+                .thenReturn(office);
+
+        mockMvc.perform(get("/offices/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.manager").value(office.getManager()));
+
+
+
+
+
+
+    }
+}
