3201a3656834f86eeb24c52e7dcf299c7c6ac92b
elie <inahon.elie17@yahoo.com>
2024-06-29 15:54:50 -0400

    Test office repository

--- src/test/java/com/eliehome/officeInformation/repository/OfficeRepositoryTest.java
+++ src/test/java/com/eliehome/officeInformation/repository/OfficeRepositoryTest.java
@@ -7,29 +7,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-//@DataJpaTest
-//
-//class OfficeRepositoryTest {
-//    @Autowired
-//
-//    private OfficeRepository officeRepository;
-//    @Autowired
-//    private TestEntityManager entityManager;
-//
-//    @BeforeEach
-//    void setUp() {
-//        Office office =
-//                Office.builder()
-//                        .staffName("Theodore Zougla")
-//                        .staffDepartment("DEV")
-//                        .staffPhoneNumber("513-503-6000")
-//                        .build();
-//
-//        entityManager.persist(office);
-//    }
-//    @Test
-//    public void whenFindById_thenReturnStaff(){
-//        Office office = officeRepository.findById(1L).get();
-//        assertEquals(office.getStaffName(), "Theodore Zougla");
-//    }
-//}
+@DataJpaTest
+
+class OfficeRepositoryTest {
+    @Autowired
+
+    private OfficeRepository officeRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        Office office =
+                Office.builder()
+                        .manager("Dossou Joseph")
+                        .address("03 Reinke Trail")
+                        .state("OH")
+                        .city("Cincinnati")
+                        .build();
+
+        entityManager.persist(office);
+    }
+    @Test
+    public void whenFindById_thenReturnOffice(){
+        Office office = officeRepository.findById(1L).get();
+        assertEquals(office.getManager(), "Dossou Joseph");
+    }
+
+    private void assertEquals(String manager, String dossouJoseph) {
+    }
+}
